a6d3bbd387f9c541e8c138d115c9a96cf46a69b1
MJ <jayedhoshen>
2020-02-26 13:48:05 +0600

    modified jquery4 file and complete form validation on an simple page

---
+++ jquery4/js/custom.js
@@ -0,0 +1,73 @@
+$(document).ready(function(){
+    $('#reg_form').submit(function(){
+        if(validateTextField('f_name', 'fnameInfo') && validateTextField('l_name', 'lnameInfo') && validateEmail('email', 'mailInfo') && validateNumber('phone','phoneInfo')){
+            return true;
+        }
+        else{
+            return false;
+        }
+    });
+    // Name validation   
+    function validateTextField(fieldName, Id){
+        // if it's Not valid
+        var nvalue = $('input[name=' + fieldName + ']').val();
+        if(nvalue.length < 1) {
+            $('#' + Id).text('Please enter any text').fadeIn();
+            $('#' + Id).addClass('error');
+            return false;
+        }
+        else if(isNaN(nvalue) == false){
+            $('#' + Id).text('Please enter any text').fadeIn();
+            $('#' + Id).addClass('error');
+            return false;
+        }
+        // if it's valid
+        else{
+            $('input[name=' + fieldName + ']').removeClass('error');
+            $('#' + Id).fadeOut();
+            return true;
+        }
+    };
+    // Email validation    
+    function validateEmail(fieldName, Id){
+        // testing rgular expression
+        var a = $('input[name=' + fieldName + ']').val();
+        var filter = /^[a-zA-Z0-9]+[a-zA-Z0-9_.-]+[a-zA-Z0-9_-]+@[a-zA-Z0-9]+[a-zA-Z0-9_.-]+[a-zA-Z0-9_-].[a-z]{2,4}$/;
+        //if it's valid email
+        if(filter.test(a)) {
+            //$('#' + fieldName).removeClass('error');
+            $('#' + Id).fadeOut();
+            $('#' + Id).removeClass('error');
+            return true;
+        }
+        //if it's Not valid
+        else{
+            $('#' + Id).text('Please Type a valid e-mail address');
+            $('#' + Id).addClass('error');
+            return false;
+        }
+    };
+    // Phone number validation    
+    function validateNumber(fieldName, Id) {
+        // if it's Not valid
+        var value = $('input[name=' + fieldName + ']').val();
+
+        if(isNaN(value)) {
+            $('#' + Id).text('Only number please, no text allowed');
+            $('#' + Id).addClass('error');
+            return false;
+        }
+        else if(value < 1 || value == ''){
+            $('#' + Id).text('Please fill the field').fadeIn();
+            $('#' + Id).addClass('error');
+            return false;
+        }
+        // if it's valid
+        else{
+            $('input= ' + fieldName + ']').removeClass('error');
+            $('#' + Id).fadeOut();
+            $('#' + Id).removeClass('error');
+            return true;
+        }
+    };
+});
