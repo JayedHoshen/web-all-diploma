22a83499633e6d4699b7464469fc52829bbd8668
MJ <jayedhoshen>
2020-03-03 13:45:47 +0600

    add some file jquery8

--- jquery8/js/custom.js
+++ jquery8/js/custom.js
@@ -1,30 +1,13 @@
-// tab slider jquery code
-$(document).ready(function(){ // Default view
-    $('#current').show(); 
-    $('#tab_bar li:nth-child(1) a').addClass('running');
-    $('#favorite,#all_time').hide();
-
-    $('#tab_bar li:nth-child(1) a').click(function(event){ // Fire when current hit clicks 
-        event.preventDefault();
-        $(this).addClass('running');
-        $('#tab_bar li:nth-child(2) a,#tab_bar li:nth-child(3) a').removeClass('running');
-        $('#current').fadeIn(500);
-        $('#favorite,#all_time').hide();
-    });
-
-    $('#tab_bar li:nth-child(2) a').click(function(event){ // Fire when all time clicks 
-        event.preventDefault();
-        $(this).addClass('running');
-        $('#tab_bar li:nth-child(1) a,#tab_bar li:nth-child(3) a').removeClass('running');
-        $('#favorite').fadeIn(500);
-        $('#current,#all_time').hide();
-    });
-
-    $('#tab_bar li:nth-child(3) a').click(function(event){ // Fire when favorite clicks 
-        event.preventDefault();
-        $(this).addClass('running');
-        $('#tab_bar li:nth-child(1) a,#tab_bar li:nth-child(2) a').removeClass('running');
-        $('#all_time').fadeIn(500);
-        $('#favorite,#current').hide();
+$(document).ready(function(){
+    $('#content_pane a').click(function(e){
+        var url = $(this).attr('href');
+        e.preventDefault();
+        $('#container')
+        .html('<img class="loader" src="images/spinner.jpg" alt=""> Loading...')
+        .load(url, function(response, status, xhr){
+            if(status == 'error') {
+                $(this).html(xhr.status+' '+xhr.statusText);
+            }
+        });
     });
 });
